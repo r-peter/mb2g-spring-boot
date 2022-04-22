c5026c91e8e64d5fbdbca0448471009e3babb8ec
Richards Peter <richards.peter@outlook.com>
2022-04-22 01:01:10 -0400

    Multi module project after avoiding repackaging of web-model

---
+++ web-model/src/main/java/guru/springframework/springboot/model/User.java
@@ -0,0 +1,12 @@
+package guru.springframework.springboot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
